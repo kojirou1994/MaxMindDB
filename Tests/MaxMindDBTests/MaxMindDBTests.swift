b120742fa76f673231892dcfb4effa778e2321c1
Kojirou <Kojirouhtc@gmail.com>
2018-02-19 13:45:52 +0800

    update test

--- Tests/MaxMindDBTests/MaxMindDBTests.swift
+++ Tests/MaxMindDBTests/MaxMindDBTests.swift
@@ -2,14 +2,22 @@ import XCTest
 @testable import MaxMindDB
 
 class MaxMindDBTests: XCTestCase {
-    let db = try! MaxMindDB(mmdbPath: "/Users/Kojirou/Downloads/GeoLite2-City_20180102/GeoLite2-City.mmdb")
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+    let db = try! MaxMindDB(mmdbPath: "./GeoLite2-City.mmdb")
+    
+    func generateRandomIP() -> String {
+        #if os(macOS)
+        return "\(arc4random_uniform(256)).\(arc4random_uniform(256)).\(arc4random_uniform(256)).\(arc4random_uniform(256))"
+        #elseif os(Linux)
+        return "\(random() % (256)).\(random() % (256)).\(random() % (256)).\(random() % (256))"
+        #endif
+    }
+    
+    func testDB() {
+        
+        
         for _ in 0..<120000 {
             do {
-                _ = try db.lookupResult(ip: "\(arc4random() % 256).\(arc4random() % 256).\(arc4random() % 256).\(arc4random() % 256)")
+                _ = try db.lookupResult(ip: generateRandomIP())
             } catch {
                 print(error)
             }
@@ -18,6 +26,6 @@ class MaxMindDBTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testDB),
     ]
 }
