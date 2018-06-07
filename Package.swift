e3abaab3e1909ae9abfedb7f68c900fbfc0c2e2d
Kojirou <Kojirouhtc@gmail.com>
2018-06-07 20:05:25 +0800

    swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,26 +1,28 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "MaxMindDB",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MaxMindDB",
             targets: ["MaxMindDB"]),
+        .executable(
+            name: "MaxMindDB-cli",
+            targets: ["MaxMindDB-cli"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/kojirou1994/Clibmaxminddb", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MaxMindDB",
             dependencies: []),
+        .target(
+            name: "MaxMindDB-cli",
+            dependencies: ["MaxMindDB"]),
         .testTarget(
             name: "MaxMindDBTests",
             dependencies: ["MaxMindDB"]),
