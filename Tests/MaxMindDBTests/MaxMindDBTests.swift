71e7ef8eced0b19483fa6795d9742cb42b3b9941
kojirou <kojirouhtc@gmail.com>
2021-05-27 03:26:07 +0800

    update test

--- Tests/MaxMindDBTests/MaxMindDBTests.swift
+++ Tests/MaxMindDBTests/MaxMindDBTests.swift
@@ -2,29 +2,20 @@ import XCTest
 @testable import MaxMindDB
 
 class MaxMindDBTests: XCTestCase {
-    let testDB = try! MaxMindDB(mmdbPath: "/Users/kojirou/Projects/MaxMindDB/GeoLite2-City.mmdb")
-    let ip = "223.5.5.5"
-    
-    override func setUp() {
-        super.setUp()
-    }
+  let testDB = try! MaxMindDB(mmdbPath: #filePath.dropLast(#fileID.count) + "../GeoLite2-City.mmdb")
+  let ip = "223.5.5.5"
 
-    func testGetResult() {
-        XCTAssertNoThrow(try testDB.lookupResult(ip: ip))
-        measure {
-            _ = try! testDB.lookupResult(ip: ip)
-        }
+  func testGetResult() {
+    XCTAssertNoThrow(try testDB.lookupResult(ip: ip))
+    measure {
+      _ = try! testDB.lookupResult(ip: ip)
     }
-    
-    func testGetJSON() {
-        XCTAssertNoThrow(try testDB.lookupJSON(ip: ip))
-        measure {
-            _ = try! testDB.lookupJSON(ip: ip)
-        }
-    }
+  }
 
-    static var allTests = [
-        ("testGetJSON", testGetJSON),
-        ("testGetResult", testGetResult),
-    ]
+  func testGetJSON() {
+    XCTAssertNoThrow(try testDB.lookupJSON(ip: ip))
+    measure {
+      _ = try! testDB.lookupJSON(ip: ip)
+    }
+  }
 }
